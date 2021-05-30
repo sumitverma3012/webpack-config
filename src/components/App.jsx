8aed002700286400de2a30e352ddf8d3f614f75f
Sumit V <sumit.v@logmein.com>
2021-05-30 16:06:44 +0530

    images handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import '../styles/index.scss';
 import Recipes from './Recipes';
+import sword from '../images/swc-sword.png';
+import swordSVG from '../images/sword.svg'
 
 const App = () => {
     return (
@@ -10,6 +12,8 @@ const App = () => {
                     <h1>React - App.jsx</h1>
                 </section>
             </main>
+            <img src={sword} alt="sword" width="250" />
+            <img src={swordSVG} alt="sword" width="250" />
             <Recipes />
         </>
     )
